1150f77c868f1d4a45e4fa580b10a20483fba825
Agustin Savino <carpasavino@gmail.com>
2022-10-02 14:14:32 -0300

    Se cambiaron los datos de la tabla y del ingreso de datos, se limpió el codigo tambien

--- main.js
+++ main.js
@@ -1,39 +1,39 @@
 //URL de la API
 const URL = 'https://jsonplaceholder.typicode.com/users';
 
-//Relacionamos variables con etiquetas para poder manipular el DOM
 const table = document.querySelector('#initialTable');
-const divTable = document.querySelector('#list');
+const divTable = document.querySelector('#divTable');
 const tableContainer = document.querySelector('#table-container');
 
-const createUser = document.querySelector('#userCreation');
+// ----
 
 function createUserRow(users){
     //Creamos las filas mediante los datos de la API o de los valores introducidos por el usuario
     users.forEach(user => {
-        const tdCompany = document.createElement('td');
-        tdCompany.classList.add('tittleColumn');
-        tdCompany.innerHTML = user.company.name;
 
-        const tdPhone = document.createElement('td');
-        tdPhone.classList.add('tittleColumn');
-        tdPhone.innerHTML = user.phone;
+        const tdName = document.createElement('td');
+        tdName.classList.add('columnData');
+        tdName.innerHTML = user.name;
 
-        const tdAddres = document.createElement('td');
-        tdAddres.classList.add('tittleColumn');
-        tdAddres.innerHTML = user.address.street;
+        const tdUsername = document.createElement('td');
+        tdUsername.classList.add('columnData');
+        tdUsername.innerHTML = user.username;
 
         const tdEmail = document.createElement('td');
-        tdEmail.classList.add('tittleColumn');
+        tdEmail.classList.add('columnData');
         tdEmail.innerHTML = user.email;
 
-        const tdId = document.createElement('td');
-        tdId.classList.add('tittleColumn');
-        tdId.innerHTML = user.id;
+        const tdPhone = document.createElement('td');
+        tdPhone.classList.add('columnData');
+        tdPhone.innerHTML = user.phone;
+
+        const tdWeb = document.createElement('td');
+        tdWeb.classList.add('columnData');
+        tdWeb.innerHTML = user.website;
 
         const trData = document.createElement('tr');
 
-        trData.append(tdId, tdEmail, tdAddres, tdPhone, tdCompany);
+        trData.append(tdName, tdUsername, tdEmail, tdPhone, tdWeb);
         table.append(trData);
         divTable.append(table);
         tableContainer.append(divTable);
@@ -53,15 +53,11 @@ async function getUsersData(){
 function createNewUser(){
     //Creamos un array que contenga un objeto para poder hacer funcionar el forEach a la hora de querer crear la fila del nuevo usuario
     const user = [{
-        id: prompt('Ingrese su id: '),
-        email: prompt('Ingrese su email: '),
-        address:{
-            street: prompt('Ingrese su direccion: '),
-        },
-        phone: prompt('Ingrese su telefono: '),
-        company:{
-            name: prompt('Ingrese el nombre de su compania: '),
-        },
+        name: prompt('Name: '),
+        username: prompt('Username: '),
+        email: prompt('Email: '),
+        phone: prompt('Phone: '),
+        website: prompt('Website:'),
     }];
     
     createUserRow(user);
